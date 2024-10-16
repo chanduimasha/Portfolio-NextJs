4fa2b5122f54ba373db89677b1a57b10a6733c5e
Chandu Imasha <chandu.2019289@iit.ac.lk>
2024-10-16 12:02:08 +0530

    Created Contact Page

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -1,7 +1,27 @@
-const Work = () => {
-  return (
-    <div className="text-white">Work page</div>
-  )
-}
+"use client";
 
-export default Work
+import { motion } from "framer-motion";
+import React, { useState } from "react";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+
+import { BsArrowUpRight, BsGithub } from "react-icons/bs";
+
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+
+import Link from "next/link";
+import Image from "next/image";
+
+
+
+const Work = () => {
+  return <div className="text-white">Work page</div>;
+};
+
+export default Work;
